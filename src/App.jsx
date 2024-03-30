f4fd50b7a8dd050d9086648ce08642b98106f691
Huy Lam <lamhongdanghuy@gmail.com>
2024-03-30 00:35:05 -0500

    little bit content on landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
+import './index.css';
 import Header from './Header';
 import LandingPage from './LandingPage';
 
--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import React from 'react';
 
 function LandingPage() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    // Assuming this div is the container for your page content
+    <div className="flex flex-col md:flex-row justify-between items-start md:items-center min-h-screen">
+      <div className="pt-16 md:pt-0 md:w-1/2 px-8"> {/* Adjust padding-top (pt-16) as needed */}
+        <h1 className="text-3xl font-bold">Hi there, my name is Huy Lam.</h1>
+        <h2 className="text-xl mt-4">Welcome to my portfolio</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <img src='../public/sel.jpg' className="w-1/4 rounded-full border-2 border-gray-300 shadow-lg m-4" alt="Huy Lam"/>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
